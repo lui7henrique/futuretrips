c594c74992403fc8083c61b26a1a548a27c63931
Luiz Henrique <7henrique18@gmail.com>
2021-08-09 18:17:02 -0300

    Initial commit from Create Next App

---
+++ src/pages/_app.tsx
@@ -0,0 +1,25 @@
+import { AppProps } from "next/app"
+import Head from "next/head"
+import { GlobalStyles } from "styles/global"
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>React Avançado - Boilerplate</title>
+        <link rel="shortcut icon" href="/icon.png" />
+        <link rel="apple-touch-icon" href="/icon.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#06092B" />
+        <meta
+          name="description"
+          content="A simple project starter to work with Typescript, React, NextJS and Styled Components"
+        />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default MyApp
